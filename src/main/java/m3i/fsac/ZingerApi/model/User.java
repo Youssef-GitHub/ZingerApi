4db5e5c3bb68b2168194dfe238d9b850d4449dd5
Youssef-GitHub <ermilijosef@gmail.com>
2022-04-03 14:06:54 +0000

    DBref has been added.

--- src/main/java/m3i/fsac/ZingerApi/model/User.java
+++ src/main/java/m3i/fsac/ZingerApi/model/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -26,6 +27,7 @@ public class User {
     private Date createdAt;
     private Boolean isBlocked;
     private Boolean isAdmin;
+    @DBRef
     private List<Post> posts;
     private List<Comment> comments;
 
